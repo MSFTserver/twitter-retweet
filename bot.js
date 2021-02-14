8aa15c77f66f3b4258ab581b5667cf164aba6c2f
MSFTserver <msftserver@gmail.com>
2021-02-13 17:14:31 -0800

    add retweet checks and reformat code

--- bot.js
+++ bot.js
@@ -17,33 +17,33 @@ let T = new Twit(config.keys); //create twitter client
 
 //fetches the list and grabs its user IDs passing it to the stream listening function
 function getListMembers() {
-    let memberIDs = [];
-    console.log('Checking Boarding Passes...\n');
+  let memberIDs = [];
+  console.log('Checking Boarding Passes...\n');
 	T.get('lists/members',{
     owner_screen_name: config.me,
     list_id: config.myList,
-	count: 5000 //max limit default is 20
+	  count: 5000 //max limit default is 20
   },
-    function(error, data){
-        if (!error) {
-            // loops over ids of users from a list
-            for (let i=0; i < data.users.length; i++) {
-                // pushes ids to array
-                memberIDs.push(data.users[i].id_str);
-            }
-            let rocketBoarding =
-            ascii.rocketBoardingTop+
-            '             |:Boarded|'+'\n'+
-            '             |:  '+memberIDs.length+'   |'+'\n'+
-            '   +         |: Users |          *'+'\n'+
-            ascii.rocketBoardingBottom
-            console.log(rocketBoarding)
-            startListen(memberIDs);
-        } else {
-            console.log(error);
-            console.log(data);
-        }
-    })
+  function(error, data){
+    if (!error) {
+      // loops over ids of users from a list
+      for (let i=0; i < data.users.length; i++) {
+        // pushes ids to array
+        memberIDs.push(data.users[i].id_str);
+      }
+      let rocketBoarding =
+      ascii.rocketBoardingTop+
+      '             |:Boarded|'+'\n'+
+      '             |:  '+memberIDs.length+'   |'+'\n'+
+      '   +         |: Users |          *'+'\n'+
+      ascii.rocketBoardingBottom
+      console.log(rocketBoarding)
+      startListen(memberIDs);
+    } else {
+      console.log(error);
+      console.log(data);
+    }
+  })
 }
 // listens to the steam with filter on accounts filtered in from a twitter list
 function startListen(users) {
@@ -81,54 +81,64 @@ function startListen(users) {
     let tweetText = tweet.text;
     let tweetID = tweet.id_str;
     let tweetUser = tweet.user.name;
-      // checks if tweet author is on users list.
-      // checks if tweet is a in_reply_to_user_id_str
-      // checks if tweet has already been retweeted
-      // if output [true,false,false] extract info
-      if (users.indexOf(tweet.user.id_str) > -1 && !tweet.in_reply_to_user_id_str && !tweet.retweeted) {
+    let retweeted = false;
+    // checks if tweet author is on users list.
+    // checks if tweet is a in_reply_to_user_id_str
+    // checks if tweet has already been retweeted
+    // if output [true,false,false] extract info
+    if (users.indexOf(tweet.user.id_str) > -1 && !tweet.in_reply_to_user_id_str && !tweet.retweeted) {
+      // checks if tweet is truncated to a char limit.
+      // if output [true] sets tweet text to extended version.
+      if (tweet.truncated){
+        tweetText = tweet.extended_tweet.full_text;
+      }
+      // checks if tweet is a retweet
+      // if output [true] set id and text to retweeted stats
+      if(tweet.retweeted_status){
+        tweetID = tweet.retweeted_status.id_str;
+        tweetText = tweet.retweeted_status.text;
+        tweetUser = tweet.retweeted_status.user.name;
         // checks if tweet is truncated to a char limit.
         // if output [true] sets tweet text to extended version.
-        if (tweet.truncated){
-          tweetText = tweet.extended_tweet.full_text;
+        if(tweet.retweeted_status.truncated){
+          tweetText = tweet.retweeted_status.extended_tweet.full_text;
         }
-        // checks if tweet is a retweet
-        // if output [true] set id and text to retweeted stats
-        if(tweet.retweeted_status){
-          tweetID = tweet.retweeted_status.id_str;
-          tweetText = tweet.retweeted_status.text;
-          tweetUser = tweet.retweeted_status.user.name;
-          // checks if tweet is truncated to a char limit.
-          // if output [true] sets tweet text to extended version.
-          if(tweet.retweeted_status.truncated){
-            tweetText = tweet.retweeted_status.extended_tweet.full_text;
-          }
-        }
-        //checks if tweetText contains any words from Accepted words list
-        let regexAccept = new RegExp(config.regexAccept, 'gi');
-        const accept = regexAccept.test(tweetText);
-        //checks if tweetText contains any words from Rejected words list
-        let regexReject = new RegExp(config.regexReject, 'gi');
-        const reject = regexReject.test(tweetText)
-        if (accept && !reject){
-          // Attempts to retweet by ID
-          T.post('statuses/retweet/:id', { id: tweetID }, function (err, data, response) {
-            if(!err){
-              // Attempts to Favorite by ID
-              T.post('favorites/create', { id: tweetID }, function (err, data, response) {
-                if(!err){
-                  console.log('Retweeted '+tweetUser+': '+tweetText);
-                } else {
-                  console.log('Favorite Failed!');
-                  console.log(err);
-                }
-              })
-            } else {
-              console.log('Retweet Failed!');
-              console.log(err);
-            }
-          })
+        // checks if tweet has already been retweeted.
+        // if output [true] sets retweeted to 1.
+        if(tweet.retweeted_status.retweeted){
+          retweeted = true;
         }
       }
+      //checks if tweetText contains any words from Accepted words list
+      let regexAccept = new RegExp(config.regexAccept, 'gi');
+      const accept = regexAccept.test(tweetText);
+      //checks if tweetText contains any words from Rejected words list
+      let regexReject = new RegExp(config.regexReject, 'gi');
+      const reject = regexReject.test(tweetText)
+      // checks if the tweet has any keywords we want
+      // checks if the tweet has any keywords we dont want
+      // checks if the tweet has already been retweeted
+      // if output [true,false,false] retweet and like status
+      if (accept && !reject && !retweet){
+        // Attempts to retweet by ID
+        T.post('statuses/retweet/:id', { id: tweetID }, function (err, data, response) {
+          if(!err){
+            // Attempts to Favorite by ID
+            T.post('favorites/create', { id: tweetID }, function (err, data, response) {
+              if(!err){
+                console.log('Retweeted '+tweetUser+': '+tweetText);
+              } else {
+                console.log('Favorite Failed!');
+                console.log(err);
+              }
+            })
+          } else {
+            console.log('Retweet Failed!');
+            console.log(err);
+          }
+        })
+      }
+    }
   });
 
   // watches limit event
